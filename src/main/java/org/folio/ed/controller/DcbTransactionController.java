72d2dc5f0a4e6fe771918803380ceafc04d4ba27
Adesh_Singh <Adesh_Singh@epam.com>
2023-09-19 20:09:40 +0530

    [EDGEDCB-2]-added endoints

--- src/main/java/org/folio/ed/controller/DcbTransactionController.java
+++ src/main/java/org/folio/ed/controller/DcbTransactionController.java
@@ -1,6 +1,6 @@
 package org.folio.ed.controller;
 
-import org.folio.ed.domain.dto.CreateCirculationRequestRequest;
+import org.folio.ed.domain.dto.CreateDCBTransactionRequest;
 import org.folio.ed.domain.dto.TransactionStatus;
 import org.folio.ed.domain.dto.TransactionStatusResponse;
 import org.folio.ed.rest.resource.TransactionsApi;
@@ -19,18 +19,18 @@ public class DcbTransactionController implements TransactionsApi {
   private final DcbTransactionService dcbTransactionService;
 
   @Override
-  public ResponseEntity<TransactionStatusResponse> getTransactionStatus(String dcbTransactionId) {
+  public ResponseEntity<TransactionStatusResponse> getDCBTransactionStatus(String dcbTransactionId) {
       return dcbTransactionService.getDcbTransactionStatus(dcbTransactionId);
   }
 
   @Override
-  public ResponseEntity<TransactionStatus> createCirculationRequest(String dcbTransactionId, CreateCirculationRequestRequest createCirculationRequestRequest) {
+  public ResponseEntity<TransactionStatus> createDCBTransaction(String dcbTransactionId, CreateDCBTransactionRequest createDCBTransactionRequest) {
     //Need to be developed
     return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(new TransactionStatus());
   }
 
   @Override
-  public ResponseEntity<TransactionStatusResponse> updateTransactionStatus(String dcbTransactionId, TransactionStatus transactionStatus) {
+  public ResponseEntity<TransactionStatusResponse> updateDCBTransactionStatus(String dcbTransactionId, TransactionStatus transactionStatus) {
     //Need to be developed
     return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(new TransactionStatusResponse());
   }
