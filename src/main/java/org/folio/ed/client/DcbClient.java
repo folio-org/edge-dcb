34acf6bd543ccba769954ef2ce7e1d26ab85b88e
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2024-04-30 17:29:53 +0530

    EDGEDCB-20 Implement GET API for transaction updates with pagination

--- src/main/java/org/folio/ed/client/DcbClient.java
+++ src/main/java/org/folio/ed/client/DcbClient.java
@@ -4,12 +4,16 @@ import org.folio.ed.client.config.OkapiFeignClientConfig;
 import org.folio.ed.domain.dto.DcbTransaction;
 import org.folio.ed.domain.dto.TransactionStatus;
 import org.folio.ed.domain.dto.TransactionStatusResponse;
+import org.folio.ed.domain.dto.TransactionStatusResponseCollection;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.OffsetDateTime;
 
 @FeignClient(name = "transactions", configuration = OkapiFeignClientConfig.class)
 public interface DcbClient {
@@ -23,4 +27,8 @@ public interface DcbClient {
   @PutMapping(value = "/{dcbTransactionId}/status")
   TransactionStatusResponse updateTransactionStatus(@PathVariable("dcbTransactionId") String dcbTransactionId,
                                                                       @RequestBody TransactionStatus transactionStatus);
+
+  @GetMapping(value = "/status")
+  TransactionStatusResponseCollection getTransactionStatusList(@RequestParam("fromDate") OffsetDateTime fromDate, @RequestParam("toDate") OffsetDateTime toDate,
+                                                               @RequestParam("pageNumber") int pageNumber, @RequestParam("pageSize") int pageSize);
 }
